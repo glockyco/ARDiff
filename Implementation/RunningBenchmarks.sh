222cd8ca4af11c0f3db0db954f20c95476749e00
Johann Glock <johann.glock@aau.at>
2022-10-29 10:52:46 +0200

    Increase depth limit of base tools to 10
    This ensures that both the base tools and the diff tools use the
    same depth limit again.

--- Implementation/RunningBenchmarks.sh
+++ Implementation/RunningBenchmarks.sh
@@ -264,7 +264,7 @@ for d1 in ../benchmarks/* ; do
             oldV="${d3}/oldV.java"
             newV="${d3}/newV.java"
 
-            base_command="timeout --verbose --foreground ${timeout}s java -jar '${BASE_JAR_PATH}' --path1 ${oldV} --path2 ${newV} ${configurations[$i]}"
+            base_command="timeout --verbose --foreground ${timeout}s java -jar '${BASE_JAR_PATH}' --path1 ${oldV} --path2 ${newV} ${configurations[$i]} --b 10"
 
             if [ "$print_commands" = true ] ; then
               printf "\n%s" "${base_command}"
